534f4db37ea40d9d2a59fa939b333186b9608b38
Chris Ngan <chris.ngan24@gmail.com>
2014-04-08 12:57:09 -0400

    major overhaul

--- scripts/xamppCopyMac.sh
+++
@@ -1,4 +0,0 @@
-lessc ../v2/css/less/main.less  ../v2/css/main.css 
-rm -rf /Applications/XAMPP/htdocs/personal-website
-cp -r ../v1 /Applications/XAMPP/htdocs/personal-website
-cp -r ../v2 /Applications/XAMPP/htdocs/personal-website
