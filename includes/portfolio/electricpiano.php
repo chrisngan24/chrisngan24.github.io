aaff83a9e8a667a57bd3a7317762703377fcc838
Chris Ngan <chris.ngan24@gmail.com>
2014-05-18 16:29:19 -0400

    added video for piano

--- includes/portfolio/electricpiano.php
+++ includes/portfolio/electricpiano.php
@@ -5,7 +5,7 @@
       <img src="images/electricpiano.png" class="img-polaroid">
     </div>
     <div class="back">
-      <p>This was a final project for a lab course I took in my first year of Engineering at Waterloo.  It uses an FPGA board to control the frequency of two speakers by simply sending a binary signal at controlled time intervals.  This oscillation of high and low created the sweet tones of an electric piano.</p>
+      <p>This was a final project for a lab course I took in my first year of Engineering at Waterloo.  It uses an FPGA board to control the frequency of two speakers by simply sending a binary signal at controlled oscillations. <a href="https://www.youtube.com/watch?v=fms_JRrR790" target="_blank">Here's</a> the demo video!</p>
     </div>
   </div>
 </div>
--- includes/portfolio/robford.php
+++ includes/portfolio/robford.php
@@ -5,7 +5,7 @@
       <img src="images/robford.png" class="img-polaroid">
     </div>
     <div class="back">
-      <p>Co-created the viral game at HackNY.  Responsible for design and implementation of JavaScript code.  Received over 140,000 plays and international media coverage (like this <a target="_blank" href="http://www.cbc.ca/newsblogs/yourcommunity/2014/04/rob-ford-crackathon-video-game-lights-up-the-web.html">article</a>). Try it now and <a target="_blank" href="http://robfordthegame.com">party like Rob Ford</a>!</p>
+      <p>Co-created the viral game at HackNY.  Received over 140,000 plays and international media coverage (like <a target="_blank" href="http://www.cbc.ca/newsblogs/yourcommunity/2014/04/rob-ford-crackathon-video-game-lights-up-the-web.html">this article</a>). Responsible for design and implementation of JavaScript code. Try it now and <a target="_blank" href="http://robfordthegame.com">party like Rob Ford</a>!</p>
       <a target="_blank" href="https://github.com/nmost/crackathon">Github</a>
     </div>
   </div>
